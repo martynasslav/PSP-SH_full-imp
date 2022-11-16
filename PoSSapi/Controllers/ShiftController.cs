2fcb3d84730fe010184961bbc4c154520a65acb7
geigosam <56157793+geigosam@users.noreply.github.com>
2022-11-16 15:35:19 +0200

    Added Shift Controller

--- PoSSapi/Controllers/ShiftController.cs
+++ PoSSapi/Controllers/ShiftController.cs
@@ -8,15 +8,41 @@ namespace PoSSapi.Controllers
     [Route("[controller]")]
     public class ShiftController : GenericController<Shift>
     {
+        private class ReturnObject {
+            public int totalShifts {get; set;}
+            public Shift[] ShiftList {get; set;}
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet()]
-        public ActionResult GetAll([FromQuery] string? employeeId)
+        public ActionResult GetAll([FromQuery] string? employeeId, [FromQuery] int itemsPerPage=10, [FromQuery] int pageNum=0)
         {
-            var objectList = new Shift[] { RandomGenerator.GenerateRandom<Shift>() };
-            if (employeeId != null)
-            {
-                objectList[0].EmployeeId = employeeId;
+            if (itemsPerPage <= 0) {
+                return BadRequest("itemsPerPage must be greater than 0");
             }
-            return Ok(objectList);
+            if (pageNum < 0) {
+                return BadRequest("pageNum must be 0 or greater");
+            }
+
+            int totalShifts = 20;
+            if (itemsPerPage * pageNum >= totalShifts) {
+                return NotFound("Requested page does not exist");
+            }
+
+            int itemsToDisplay = Math.Min(totalShifts - 1, itemsPerPage * (pageNum + 1) - 1) % itemsPerPage + 1;
+            var objectList = new Shift[itemsToDisplay];
+            for (int i = 0; i < itemsToDisplay; ++i) {
+                objectList[i] = RandomGenerator.GenerateRandom<Shift>();
+
+                if (employeeId != null) {
+                    objectList[i].EmployeeId = employeeId;
+                }
+            }
+
+            ReturnObject returnObject = new ReturnObject {totalShifts = totalShifts, ShiftList = objectList};
+            return Ok(returnObject);
         }
     }
 }
