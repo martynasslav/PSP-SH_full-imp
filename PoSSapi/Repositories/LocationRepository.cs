a4bdd4a94c57e6354dc19b289ce127f87d784884
martynasslav <martynas.slav@gmail.com>
2022-12-29 06:03:06 +0200

    Add Location repository.

---
+++ PoSSapi/Repositories/LocationRepository.cs
@@ -0,0 +1,47 @@
+using PoSSapi.Classes;
+using PoSSapi.Database;
+
+namespace PoSSapi.Repositories
+{
+    public class LocationRepository : ILocationRepository
+    {
+        private readonly DbEntities _dbEntities;
+
+        public LocationRepository(DbEntities dbEntities)
+        {
+            _dbEntities = dbEntities;
+        }
+
+        public void InsertLocation(Location location)
+        {
+            _dbEntities.Locations.Add(location);
+        }
+
+        public Location? GetLocationById(string id)
+        {
+            return _dbEntities.Locations.FirstOrDefault(l => l.Id == id);
+        }
+
+        public IEnumerable<Location> GetLocations()
+        {
+            return _dbEntities.Locations.AsEnumerable();
+        }
+
+        public void UpdateLocation(Location location)
+        {
+            var item = _dbEntities.Locations.First(l => l.Id == location.Id);
+            item.Address = location.Address;
+            item.Name = location.Name;
+        }
+
+        public void DeleteLocation(Location location)
+        {
+            _dbEntities.Locations.Remove(location);
+        }
+
+        public void Save()
+        {
+            _dbEntities.SaveChanges();
+        }
+    }
+}
