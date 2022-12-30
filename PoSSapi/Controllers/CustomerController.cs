d29be01af4b241b0ae2ad829df0df06424871699
karoluzas <79944650+karoluzas@users.noreply.github.com>
2022-12-30 21:22:43 +0200

    Add comment

--- PoSSapi/Controllers/CustomerController.cs
+++ PoSSapi/Controllers/CustomerController.cs
@@ -128,28 +128,26 @@ namespace PoSSapi.Controllers
         }
 
 
-        // TODO - this part
-        /** <summary>Gets customers payments by id</summary>
-         * <param name="id" example="">Id of the customer whose payments you wish to see</param>
-         */
-        /*[ProducesResponseType(StatusCodes.Status200OK)]
+        // TODO - This part. Need Payment implementation
+        /// <summary>Gets customers payments by id</summary>
+        /// <param name="id">Id of the customer whose payments you wish to see</param>
+        [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
         [HttpGet("{id}/spending")]
         public ActionResult GetCustomerSpendingById(string id)
         {
-            *//*var spendingDto = RandomGenerator.GenerateRandom<SpendingDto>();
+            /*var spendingDto = RandomGenerator.GenerateRandom<SpendingDto>();
             spendingDto.CustomerId = id;
-            spendingDto.Payments = new List<PaymentDto>();
+            spendingDto.Payments = new List<PaymentDto>();*/
 
-            var amountOfPayments = new Random().Next(1, 10);
+            // var amountOfPayments = new Random().Next(1, 10);
 
-            for (int i = 0; i < amountOfPayments; i++)
+            /*for (int i = 0; i < amountOfPayments; i++)
             {
                 //WHY ARENT THESE RANDOM!!!???
                 spendingDto.Payments.Add(RandomGenerator.GenerateRandom<PaymentDto>());
-            }
-            *//*
+            }*/
             return Ok();
-        }*/
+        }
     }
 }
