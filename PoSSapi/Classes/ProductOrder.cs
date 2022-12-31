e537aa7736fe029914f980610ea91ffbb377bd5f
Konstantinas Bernotas <kostismin3craft@gmail.com>
2022-12-31 15:49:33 +0200

    Classes

--- PoSSapi/Classes/ProductOrder.cs
+++ PoSSapi/Classes/ProductOrder.cs
@@ -1,10 +1,18 @@
 using Enums;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+#pragma warning disable 8618
 
 namespace Classes;
 
 public class ProductOrder : Order
 {
+    [Required]
+    [ForeignKey("OrderProduct")]
     public int TableNumber { get; set; }
+    [Required]
     public decimal Tips { get; set; }
+    [Required]
     public ProductOrderType OrderType { get; set; }
 }
